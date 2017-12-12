bf140ead35766671049fa76b18244d316c291d0e
Marcus <marcus_peter@gmx.de>
2017-12-12 15:39:34 +0100

    New stored functions

---
+++ func.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        Functions for Accessibility-tools
+# Purpose:
+#
+# Author:      mape
+#
+# Created:     12/12/2017
+# Copyright:   (c) mape 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+#--Functions--#
+def rechnen ():
+    print "test"
+    print "Modul:" +__name__
