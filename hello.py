0581cf442b3428d81ee2316e0680599a7b41b33f
Marcus <marcus_peter@gmx.de>
2017-12-14 15:33:56 +0100

    Adding new Submenu

--- hello.py
+++ hello.py
@@ -26,33 +26,44 @@ def callback():
     name = askopenfilename()
     v.set(name)
 
+def matrizen():
+    root.geometry('600x250')
+    Label(root, text="Berechnung von Matrizen",font=("Calibri", 14, "bold")).place(x=300,y=0)
+    #Insert Field
+    Label(root, text="Pfad:",font=("Calibri", 11, "bold")).place(x=300,y=50)
+    e1 = Entry(root, textvariable=v)
+    e1.place(x=340,y=50,height=22)
+
+
 #Insert Menu-bar
 menubar = Menu(root)
 
 #Insert drop-down menu
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open",font=("Calibri", 11, "bold"), command=callback)
-filemenu.add_command(label="Ende",font=("Calibri", 11, "bold"), command=close_window)
+Analysen = Menu(filemenu, tearoff=0)
+filemenu.add_cascade(label="Analysen",font=("Calibri", 10, "bold"),menu=Analysen)
+Analysen.add_command(label="Matrizen",font=("Calibri", 10, "bold"),command=matrizen)
+filemenu.add_separator()
+filemenu.add_command(label="Open",font=("Calibri", 10, "bold"), command=callback)
+filemenu.add_command(label="Ende",font=("Calibri", 10, "bold"), command=close_window)
 
 zahl = Menu(menubar, tearoff=0)
 zahl.add_command(label="1",font=("Calibri", 11, "bold"),command=func.rechnen)
-zahl.add_command(label="5",font=("Calibri", 11, "bold"))
-zahl.add_command(label="13",font=("Calibri", 11, "bold"))
 
 menubar.add_cascade(label="File", menu=filemenu)
-menubar.add_cascade(label="Hilfe")
 menubar.add_cascade(label="Rechnen", menu=zahl)
+menubar.add_cascade(label="Hilfe")
 root.config(menu=menubar)
 
-#Insert Field
-Label(root, text="Pfad:",font=("Calibri", 11, "bold")).place(x=0,y=70)
+###Insert Field
+##Label(root, text="Pfad:",font=("Calibri", 11, "bold")).place(x=0,y=70)
 v = StringVar()
-e1 = Entry(root, textvariable=v)
-e1.place(x=40,y=70,height=22)
+##e1 = Entry(root, textvariable=v)
+##e1.place(x=40,y=70,height=22)
 
 #Insert logo/Pics
 logo = PhotoImage(file=os.getcwd()+"\\test.gif")
-w1 = Label(root, image=logo).place(x=0, y=100)
+w1 = Label(root, image=logo).place(x=0, y=60)
 
 #Insert Text
 explanation = """At present, only GIF and PPM/PGM
