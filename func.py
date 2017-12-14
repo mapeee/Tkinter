ad2a2a2641bf9588b79d9deee3a7100a54f39c46
Marcus <marcus_peter@gmx.de>
2017-12-14 16:57:53 +0100

    Lots of changes

--- func.py
+++ func.py
@@ -15,7 +15,16 @@ def main():
 if __name__ == '__main__':
     main()
 
+from tkFileDialog import askopenfilename
+
 #--Functions--#
 def rechnen ():
     print "test"
     print "Modul:" +__name__
+
+def ausgeben ():
+    print "rechnen"
+
+def callback(v):
+    name = askopenfilename()
+    v.set(name)
--- hello.py
+++ hello.py
@@ -16,15 +16,11 @@ import func
 #Starte Tk und definiere Fenster
 root = Tk()
 root.title("Mape's Erreichbarkeitstools")
-root.geometry('250x250')
+root.geometry('300x300')
 
 #--Functions--#
 def close_window ():
     root.destroy()
-
-def callback():
-    name = askopenfilename()
-    v.set(name)
 
 def matrizen():
     root.geometry('600x250')
@@ -34,9 +30,15 @@ def matrizen():
     e1 = Entry(root, textvariable=v)
     e1.place(x=340,y=50,height=22)
 
+    #Insert Button
+    button = Button(root, text='Starten', width=25, command=func.ausgeben).place(x=300,y=100)
+
 
 #Insert Menu-bar
 menubar = Menu(root)
+
+#Global variables
+v = StringVar()
 
 #Insert drop-down menu
 filemenu = Menu(menubar, tearoff=0)
@@ -44,7 +46,7 @@ Analysen = Menu(filemenu, tearoff=0)
 filemenu.add_cascade(label="Analysen",font=("Calibri", 10, "bold"),menu=Analysen)
 Analysen.add_command(label="Matrizen",font=("Calibri", 10, "bold"),command=matrizen)
 filemenu.add_separator()
-filemenu.add_command(label="Open",font=("Calibri", 10, "bold"), command=callback)
+filemenu.add_command(label="Open",font=("Calibri", 10, "bold"), command=lambda:func.callback(v))
 filemenu.add_command(label="Ende",font=("Calibri", 10, "bold"), command=close_window)
 
 zahl = Menu(menubar, tearoff=0)
@@ -55,22 +57,14 @@ menubar.add_cascade(label="Rechnen", menu=zahl)
 menubar.add_cascade(label="Hilfe")
 root.config(menu=menubar)
 
-###Insert Field
-##Label(root, text="Pfad:",font=("Calibri", 11, "bold")).place(x=0,y=70)
-v = StringVar()
-##e1 = Entry(root, textvariable=v)
-##e1.place(x=40,y=70,height=22)
-
 #Insert logo/Pics
-logo = PhotoImage(file=os.getcwd()+"\\test.gif")
+logo = PhotoImage(file=os.getcwd()+"\\Logo.gif")
 w1 = Label(root, image=logo).place(x=0, y=60)
 
 #Insert Text
-explanation = """At present, only GIF and PPM/PGM
-formats are supported, but an interface
-exists to allow additional image file
-formats to be added easily."""
-w2 = Label(root,justify=LEFT,text=explanation).place(x=0, y=0)
+explanation = """A toolbox to produce different
+accessibility analyses."""
+w2 = Label(root,justify=LEFT,text=explanation, font=("Calibri", 12, "bold")).place(x=0, y=0)
 
 #End
 root.mainloop()
